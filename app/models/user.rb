9fc8554131facec97267e4b02d0cb6243ba6a549
Nenad Milenkovic <milenkovicnkv@gmail.com>
2018-06-24 16:44:16 +0200

    Add avatar for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,23 @@
 class User < ApplicationRecord
+
   has_many :tasks
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  :recoverable, :rememberable, :trackable, :validatable
 
   def username
     return self.email.split('@')[0].capitalize
   end       
+ 
+  mount_uploader :avatar, AvatarUploader
+
+  #validates_processing_of :avatar
+  #validate :avatar_size_validation
+  #private
+  #  def avatar_size_validation
+  #    errors[:avatar] << "should be less than 500KB" if avatar.size > 0.5.megabytes
+  #  end
+
 end
