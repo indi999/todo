600a87fd6b42df5b356a501d92a5d89df56b6b36
Nenad Milenkovic <milenkovicnkv@gmail.com>
2018-06-29 12:09:24 +0200

    Update login and signin template and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,22 @@
 Rails.application.routes.draw do
  
-  resources :tasks
-  root to: "pages#home"
+  
+  #devise_scope :user do
+  #  root to: "devise/sessions#new"
+  #end
+  devise_for :users
+  devise_scope :user do
+    authenticated :user do
+      root 'pages#home', as: :authenticated_root
+    end
+  
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 
   get 'pages/home'
-  devise_for :users
+  resources :tasks
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
